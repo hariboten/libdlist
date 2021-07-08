ca76bdb6a3144ff072944acffd348251db4bdfd9
hariboten <botegineer@outlook.com>
2021-07-08 18:29:31 +0900

    Add comment to header file

--- include/dlist.h
+++ include/dlist.h
@@ -6,7 +6,7 @@
 /*   By: ewatanab <ewatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 17:49:11 by ewatanab          #+#    #+#             */
-/*   Updated: 2021/07/08 18:25:42 by ewatanab         ###   ########.fr       */
+/*   Updated: 2021/07/08 18:28:26 by ewatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,32 @@ struct			s_dlist
 	t_dlist	*prev;
 };
 
+/*
+ * dlist.c
+ */
 t_dlist			*dlist_new(void *content);
 t_dlist			*dlist_front(t_dlist *lst);
 t_dlist			*dlist_back(t_dlist *lst);
+size_t			dlist_size(t_dlist *lst);
+
+/*
+ * push_pop.c
+ */
 void			dlist_push_front(t_dlist **lst, t_dlist *newlst);
 void			dlist_push_back(t_dlist **lst, t_dlist *newlst);
 void			dlist_insert(t_dlist **lst, t_dlist *newlst);
 void			dlist_pop_front(t_dlist *lst, void (*del)(void *));
 void			dlist_pop_back(t_dlist *lst, void (*del)(void *));
+
+/*
+ * delete.c
+ */
 void			dlist_erase(t_dlist *lst, void (*del)(void *));
 void			dlist_clear(t_dlist **lst, void (*del)(void *));
+
+/*
+ * operate.c
+ */
 void			dlist_swap(t_dlist **a, t_dlist **b);
-size_t			dlist_size(t_dlist *lst);
 
 #endif
